19014f04367ab0279f6107814e3c7b80efc4b87c
Pavel Shut <pavel.shut@10clouds.com>
2022-10-21 17:34:59 +0200

    check connection data

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
-fetch('/').then(r => {
-  let msg = ''
-  if (r.ok){
-    msg = 'You are ONline!'
-  } else {
-    msg = 'You are OFFline :/'
-  }
-  return msg
-}).catch((reason) => {
-  return "Hi dino!"
-}).then((msg) => {
+function getConnectionData() {
+  return [
+    ['type'],
+    ['effectiveType'],
+    ['saveData'],
+    // 'rtt',
+  ].reduce((acc, [key, fn]) => {
+    acc[key] = navigator.connection[key]
+    return acc
+  }, {})
+}
+
+function setConnectionData(data) {
+  connection.innerHTML = JSON.stringify(data,null, 2)
+}
+const setMessage = (msg) => {
   onlinestatus.innerHTML = msg
-})
+}
+
+async function checkFetch() {
+  return fetch('/').then(r => {
+    let msg = ''
+    if (r.ok){
+      msg = 'You are ONline!'
+    } else {
+      msg = 'You are OFFline :/'
+    }
+    return msg
+  }).catch((reason) => {
+    return "Hi dino!"
+  })
+}
+
+const doChecks = async (e) => {
+  setConnectionData(getConnectionData())
+  await checkFetch().then(setMessage)
+};
+
+navigator?.connection?.addEventListener('change', doChecks);
+window.addEventListener('load', doChecks)
+
